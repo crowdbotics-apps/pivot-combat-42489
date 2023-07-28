c0eaeed41960b02db4eb09d8a290c892277e6155
Crowdbotics <team@crowdbotics.com>
2023-07-28 19:45:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pivot-combat-42489",
+  url: "https://pivot-combat-42489.botics.co",
+  api: "https://pivot-combat-42489.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
